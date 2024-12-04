42060bb627d10ddcee4a99aaf68df826bd0d9830
Maxime LEGRAND <maxime.legrand@bessarion.fr>
2024-12-04 16:09:37 +0100

    feat(Seo): useSeoMeta with data of Sanity

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -8,17 +8,28 @@
 
 <script setup lang="ts">
 import type { SanityDocument } from "@sanity/client";
-// import imageUrlBuilder from "@sanity/image-url";
-// import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 const route = useRoute();
 
 const { data: post } = await useSanityQuery<SanityDocument>(POST_QUERY, { slug: route.params.slug });
+const { projectId, dataset } = useSanity().client.config();
+const urlFor = (source: SanityImageSource) =>
+	projectId && dataset
+	? imageUrlBuilder({ projectId, dataset }).image(source)
+	: null;
 
 if(!post.value) {
 	throw createError({ statusCode: 404, message: "Post not found" });
 }
+
+useSeoMeta({
+	title: `${post.value?.title} | Tracking App`,
+	description: 'Découvrez tous les articles de notre blog',
+	ogImage: post?.value?.image && urlFor(post?.value?.image)?.url() ? urlFor(post?.value?.image)?.url() : '/img/meta-image.png',
+});
 
 // const { projectId, dataset } = useSanity().client.config();
 // const urlFor = (source: SanityImageSource) =>
