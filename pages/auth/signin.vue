0308ab38d038daad2f44df1ebbf207f35ce54877
MaxouLeKangou <maxime.legrand@bessarion.fr>
2024-12-22 17:43:43 +0100

    fix(auth): Page Signin & Signup

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -2,13 +2,13 @@
 	<main class="auth">
 		<form method="post" class="auth__form" @submit.prevent="onSubmit">
 			<fieldset class="auth__fieldset">
-				<MyInput v-model="account.username" placeholder="Nom d'utilisateur" type="username" label="Username" size="full" class="auth__input" />
-				<MyInput v-model="account.password" placeholder="Mot de passe" type="password" label="Password" size="full" class="auth__input" />
+				<Input v-model="account.username" placeholder="Nom d'utilisateur" type="username" label="Username" full class="auth__input" />
+				<Input v-model="account.password" placeholder="Mot de passe" type="password" label="Password" full class="auth__input" />
 				<nuxt-link to="" class="auth__link -end">Mot de passe oublié ?</nuxt-link>
 			</fieldset>
 			<div class="auth__submit">
 				<p class="auth__terms">En continuant, vous acceptez nos <NuxtLink to="" class="auth__link -end">Conditions d'utilisations</NuxtLink> ainsi que notre <NuxtLink to="" class="auth__link">Charte de confidentialité</NuxtLink> !</p>
-				<MyButton type="submit" label="Se connecter" size="full" class="auth__button" />
+				<Button type="submit" label="Se connecter" full class="auth__button" />
 				<nuxt-link to="/auth/signup" class="auth__link -center">Vous n'avez pas de compte ?</nuxt-link>
 			</div>
 		</form>
