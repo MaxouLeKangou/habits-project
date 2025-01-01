c1b0a8cbc945c95043e457d7801ed1d7faada9cc
MaxouLeKangou <maxime.legrand@bessarion.fr>
2025-01-01 19:07:12 +0100

    feat(landing): Create Menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,27 @@
 <template>
 	<header class="header">
 		<NuxtLink to="/">
-			<Logo class="header__logo"/>
+			<Logo class="header__logo" />
 		</NuxtLink>
-		
-		<button>
-			<IconsHamburger class="header__menu"/>
+
+		<button @click="isMenuOpen = true" aria-label="Open menu">
+			<IconsHamburger class="header__menu" />
 		</button>
+
+		<nav class="header__nav" :class="{ 'header__nav--open': isMenuOpen }" @click="isMenuOpen = !isMenuOpen">
+			<ul>
+				<li><NuxtLink to="/">Home</NuxtLink></li>
+				<li><NuxtLink to="/dashboard">Dashboard</NuxtLink></li>
+				<li><NuxtLink to="/auth/signin">Connection</NuxtLink></li>
+			</ul>
+			<button class="header__close" aria-label="Close menu">✕</button>
+		</nav>
 	</header>
 </template>
+
+<script setup>
+const isMenuOpen = ref(false);
+</script>
 
 <style lang="scss">
 .header {
@@ -31,11 +44,13 @@
 			height: rem(85px);
 		}
 	}
-	
+
 	button {
 		background: none;
 		border: none;
 		cursor: pointer;
+		color: white;
+		font-size: 1.5rem;
 	}
 
 	&__menu {
@@ -50,6 +65,76 @@
 		@include mq('large') {
 			width: rem(40px);
 			height: rem(40px);
+		}
+	}
+
+	&__nav {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: black;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		transform: translateY(-100%);
+		opacity: 0;
+		visibility: hidden;
+		transition: all 0.3s ease-in-out;
+
+		&--open {
+			transform: translateY(0);
+			opacity: 1;
+			visibility: visible;
+		}
+
+		ul {
+			list-style: none;
+			text-align: center;
+			padding: 0;
+			margin: 0;
+
+			display: flex;
+			flex-direction: column;
+			gap: rem(20px);
+
+			li {
+
+				a {
+					text-decoration: none;
+					color: white;
+					font-size: 1.5rem;
+					transition: color 0.3s;
+
+					&:hover {
+						color: $secondary-100;
+					}
+
+					@include mq('medium') {
+						font-size: 2rem;
+					}
+
+					@include mq('large') {
+						font-size: 2.5rem;
+					}
+				}
+			}
+		}
+	}
+
+	&__close {
+		position: absolute;
+		top: rem(70px);
+		right: rem(30px);
+		background: none;
+		border: none;
+		color: white;
+		font-size: 2rem;
+		cursor: pointer;
+
+		&:hover {
+			color: $secondary-100;
 		}
 	}
 }
